9d4ff86cb980b7b4b2c576cfe1d79ae7078c73d6
emmeiwhite <emmeiwhite@gmail.com>
2025-05-30 11:44:32 +0530

    dynamic product routes

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,8 @@
+const ProductDetailPage = () => {
+  return (
+    <div>
+      <h1>Product Detail Page</h1>
+    </div>
+  )
+}
+export default ProductDetailPage
---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+const Products = () => {
+  return (
+    <section>
+      <h1>Products</h1>
+
+      <p>Product-1</p>
+      <p>Product-2</p>
+      <p>Product-3</p>
+    </section>
+  )
+}
+export default Products
