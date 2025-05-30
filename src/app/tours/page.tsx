2965916d96ab7bc33a2495818cd4ed6bd3b62f03
emmeiwhite <emmeiwhite@gmail.com>
2025-05-30 13:30:23 +0530

    adding a little delay while fetching data in Server Component

--- src/app/tours/page.tsx
+++ src/app/tours/page.tsx
@@ -8,11 +8,26 @@ type Tour = {
   image: string
 }
 
-// Fetching data should be done in a Server Component
-const ToursPage = async () => {
+const fetchTours = async () => {
+  // Adding a brief delay for loading state
+
+  await new Promise(resolve => setTimeout(resolve, 3000))
   const response = await fetch(url)
   const data: Tour[] = await response.json()
+  return data
+}
+
+// Fetching data should be done in a Server Component
+const ToursPage = async () => {
+  const data = await fetchTours()
   console.log(data)
-  return <div>ToursPage</div>
+
+  return (
+    <section>
+      {data.map(tour => {
+        return <p key={tour.id}>{tour.name}</p>
+      })}
+    </section>
+  )
 }
 export default ToursPage
